568cb9acefdfa6c084c6441ee167a1629472ec3b
Jeffrey Stokoe <admin@aelintra.com>
2026-01-24 15:50:02 -0500

    Fix: Remove 'create another' button from Call Routes modal
    The 'New' button in ListCallRoutes opens a modal, and the canCreateAnother
    property on CreateCallRoute page doesn't affect modal behavior. Need to
    explicitly disable createAnother on the CreateAction in header actions.
    
    Fixes regression where modal showed 'Create' and 'Create and create another'
    buttons even though canCreateAnother was set to false on the Create page.

--- app/Filament/Resources/CallRouteResource/Pages/ListCallRoutes.php
+++ app/Filament/Resources/CallRouteResource/Pages/ListCallRoutes.php
@@ -13,7 +13,8 @@ class ListCallRoutes extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->createAnother(false),
         ];
     }
 }
